a09910454e48a195cc2848b2ab086b05a8c6f87e
Yung-Chung Ku <ycku@csie.org>
2020-09-02 16:22:01 +0800

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,7 +23,7 @@ function loadFile(filePath) {
  	xmlhttp.open("GET", filePath, false);
  	xmlhttp.send();
 	if (xmlhttp.status==200) {
- 		result = xmlhttp.responseText;
+ 		result = xmlhttp.responseXML;
   	}
   	return result;
 }
